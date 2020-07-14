ad2029625f5858bd18144de1769a23d4aa264701
sebastienbouyt@gmail.com <sebastienbouyt@hotmail.fr>
2020-07-14 13:18:47 +0200

    ajout d'un fichier readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Minuteur
+
+Minuteur en ligne qui propose de créer ces propres minuteurs
+
+# Instalation
+
+    -   Lancer le script de création de base de donnée : minuteur.sql
+    -   Modifier la configuration de la base de donnée dans Manager.php
--- minuteur.sql
+++ minuteur.sql
@@ -1,29 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.9.2
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1:3308
--- Généré le :  mar. 14 juil. 2020 à 11:13
--- Version du serveur :  5.7.9
--- Version de PHP :  7.4.0
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données :  `minuteur`
---
-
--- --------------------------------------------------------
-
 --
 -- Structure de la table `minuteur`
 --
@@ -42,11 +16,6 @@ CREATE TABLE IF NOT EXISTS `minuteur` (
 --
 
 INSERT INTO `minuteur` (`id`, `titre`, `second`, `actif`) VALUES
-(1, 'test', 90, 1),
-(2, 'oeuf à la coque', 180, 1),
-(3, 'sport', 1800, 1);
+(1, 'Oeuf à la coque', 180, 1),
+(3, 'Sport', 1800, 1);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
